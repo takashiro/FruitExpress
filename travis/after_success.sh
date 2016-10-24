6d21058eb3ebdabad4f374d087947023758f9f5b
Takashiro <takashiro@qq.com>
2016-10-24 19:34:36 +0800

    Travis CI should deploy tag commits only

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -n $TRAVIS_TAG ]; then
+if [ "$TRAVIS_TAG" != "" ]; then
 	curl --ftp-ssl -k -T app/build/outputs/apk/app-release.apk -u $FTP_USER:$FTP_PASSWD ftp://weifruit.cn:8021/html/app/FruitExp-${TRAVIS_TAG}.apk
 	curl --ftp-ssl -k -T changelog/$TRAVIS_TAG.txt -u $FTP_USER:$FTP_PASSWD ftp://weifruit.cn:8021/html/app/changelog-${TRAVIS_TAG}.txt
 
